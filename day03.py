f3f31c27143b400128cd3f3528c03e81dc428f5c
ark <abhishekkuniyal@gmail.com>
2025-10-05 21:43:04 +0530

    sorting

---
+++ day03.py
@@ -0,0 +1,14 @@
+#sorting -
+#1.insertion sort :-
+
+def selectionSort(arr):
+    n = len(arr)
+    for i in range(0,n):
+        min = i
+        for j in range(i+1,n):
+            if arr[j] < arr[min]:
+                min = j
+        arr[i],arr[min] = arr[min],arr[i]
+    return arr
+        
+print(selectionSort([5,4,3,2,1]))
